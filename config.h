72a826c8eaaf351cfa3337826a8a34e64862b5f7
Josh Wright <wright.jjw@gmail.com>
2020-10-15 12:24:53 -0400

    new colors

--- config.h
+++ config.h
@@ -79,7 +79,7 @@ char *termname = "st-256color";
 /*
  * spaces per tab
  *
- * When you are changing this value, don't forget to adapt the »it« value in
+
  * the st.info and appropriately install the st.info in the environment where
  * you use this st version.
  *
@@ -98,43 +98,48 @@ float alpha = 0.75;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
 
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+  /* 8 normal colors */
+  [0] = "#090300", /* black   */
+  [1] = "#db2d20", /* red     */
+  [2] = "#01a252", /* green   */
+  [3] = "#fded02", /* yellow  */
+  [4] = "#01a0e4", /* blue    */
+  [5] = "#a16a94", /* magenta */
+  [6] = "#90d1f4", /* cyan    */
+  [7] = "#a5a2a2", /* white   */
 
-	[255] = 0,
+  /* 8 bright colors */
+  [8]  = "#5c5855", /* black   */
+  [9]  = "#db776f", /* red     */
+  [10] = "#60c77a", /* green   */
+  [11] = "#fdf79c", /* yellow  */
+  [12] = "#7ec5e4", /* blue    */
+  [13] = "#a1859a", /* magenta */
+  [14] = "#b9e4f4", /* cyan    */
+  [15] = "#f7f7f7", /* white   */
 
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-    "#181818",
+  /* special colors */
+  [256] = "#1d1f21", /* background */
+  [257] = "#c5c8c6", /* foreground */
 };
-
 
 /*
  * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
+ * foreground, background, cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 258;
-static unsigned int defaultcs = 256;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+static unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 257;
+
+/*
+ * Colors used, when the specific fg == defaultfg. So in reverse mode this
+ * will reverse too. Another logic would only make the simple feature too
+ * complex.
+ */
+static unsigned int defaultitalic = 7;
+static unsigned int defaultunderline = 7;
 
 /*
  * Default shape of cursor
